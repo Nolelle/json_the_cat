a2e4dfe7dc67cfde199c35ca0c467750afe50f64
Edmund Yu <eyu1@ualberta.ca>
2021-09-23 23:02:31 +0000

    Implemented index.js and breedFetcherTest.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,19 @@
 const request = require("request");
-const args = process.argv.splice(2);
-// console.log(args);
 
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${args}`,
-  function(error, response, body) {
-    if (error) {
-      console.log(error);
+const fetchBreedDescription = function (breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    function (error, response, body) {
+      const data = JSON.parse(body);
+      if (error || data[0] === undefined) {
+        callback(error, null);
+      } else {
+        const firstElement = data[0];
+        const description = firstElement.description;
+        callback(null, description);
+      }
     }
-    const data = JSON.parse(body);
-    if (data[0] === undefined) {
-      console.log(
-        "The requested breed was not found please ensure the your entry was spelt correctly or request another breed"
-      );
-      process.exit();
-    } else {
-      const firstElement = data[0];
-      console.log(firstElement.description);
-    }
-  }
-);
+  );
+};
+
+module.exports = { fetchBreedDescription, request };
